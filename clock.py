6536786f64e8b0234694c53e0178def55c2eac03
IT21314742 <it21314742@my.sliit.lk>
2025-01-12 13:17:38 +0530

    Update
    Update

--- clock.py
+++ clock.py
@@ -7,6 +7,7 @@ from PyQt5.QtCore import QTimer, QTime, Qt
 class DigitalClock(QWidget):
     def __init__(self):
         super().__init__()
+        self.time_label = QLabel(self)
         self.initUI()
     
     def initUI(self):
