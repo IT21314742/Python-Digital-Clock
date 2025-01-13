c9efe0da9026964e48ef41f0b252313d54db54a6
IT21314742 <it21314742@my.sliit.lk>
2025-01-13 21:20:51 +0530

    Update
    Update

--- clock.py
+++ clock.py
@@ -27,6 +27,7 @@ class DigitalClock(QWidget):
 
         def update_time(self):
             current_time = QTime.currentTime().toString("hh:mm:ss")
+            
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
